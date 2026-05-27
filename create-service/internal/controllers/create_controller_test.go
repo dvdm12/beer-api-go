3f3f54a709930d4c0f5923e059e8cddf878827a6
David Mantilla <dvddavid677@gmail.com>
2026-05-26 21:08:21 -0500

    test 2

--- create-service/internal/controllers/create_controller_test.go
+++ create-service/internal/controllers/create_controller_test.go
@@ -66,6 +66,10 @@ func validBeer() models.Beer {
 	}
 }
 
+func TestFail(t *testing.T) {
+	t.Fail()
+}
+
 func TestCreateController_CreateBeer_Success(t *testing.T) {
 	mock := &mockService{}
 	r := setupRouter(mock)
